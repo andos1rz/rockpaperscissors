ae3c5865d8f7b45be4aad96c06e63c5c89e75e45
andy <andrew.lockley@outlook.com>
2026-03-17 17:48:59 +1100

    wrote functions for returning computer and human choices

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,27 @@
-console.log("helloworld");
+// make computer randomly return string "rock", "paper", or "scissors" ---DONE---
+// get human input of either "rock", "paper", or "scissors" ---DONE---
+// write a system to keep score between rounds
+// write the logic to play a single round
+// write the logic to play an entire game of x rounds
+
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            return "rock"
+        case 2:
+            return "paper"
+        case 3:
+            return "scissors"
+    }
+}
+console.log(getComputerChoice())
+
+function getHumanChoice() {
+    return humanChoice.toLowerCase();
+}
+let humanChoice = prompt("Rock, paper, scissors!");
+console.log(getHumanChoice());
+
+function scoring() {
+    
+}
