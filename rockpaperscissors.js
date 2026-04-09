0d8cab55dc476579ac5d4e05462953f0d62d86ff
andy <andrew.lockley@outlook.com>
2026-04-09 12:17:50 +1000

    playGame updated to include other functions

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,70 +4,65 @@
 // write the logic to play a single round ---DONE---
 // write the logic to play an entire game of x rounds 
 
-function getComputerChoice() {
-    switch (Math.floor(Math.random() * 3) + 1) {
-        case 1:
-            return "rock"
-        case 2:
-            return "paper"
-        case 3:
-            return "scissors"
-    }
-}
-const computerChoice = getComputerChoice();
-// console.log(computerChoice);
-
-function getHumanChoice() {
-    return humanChoice.toLowerCase();
-}
-const humanChoice = prompt("Choose rock, paper, or scissors:  ");
-// console.log(getHumanChoice());
-
-function singleRound() { 
-    const win = "win";
-    const lose = "lose";
-    const draw = "draw"; 
-    const error = "error";
-    
-    if (getHumanChoice() == "rock" && computerChoice == "scissors") {
-        return win     
-    } if (getHumanChoice() == "paper" && computerChoice == "rock") {
-        return win
-    } if (getHumanChoice() == "scissors" && computerChoice == "paper") {
-        return win
-    } if (getHumanChoice() == computerChoice) {
-        return draw
-    } if (getHumanChoice() != "rock" && getHumanChoice() != "paper" && getHumanChoice() != "scissors") {
-        return error 
-    } else 
-        return lose
-}
-
 function playGame() {
-
-}
-
-function scoring() {
-    // on a human win increment the humanScore by 1, on a loss increment computerScore by 1
-    // output both scores
-    const result = singleRound();
-    let message = "";
-    if (result == "win") {
-        humanScore ++;
-        message = `${getHumanChoice()} vs. ${computerChoice}. You win!`;
-    } if (result == "lose") {
-        computerScore ++;
-        message = `${getHumanChoice()} vs. ${computerChoice}. You lose.`;
-    } if (result == "draw") {
-        message = `${getHumanChoice()} vs. ${computerChoice}. Draw! Try again.`;
-    } if (result == "error") {
-        message = "Please input a vaild choice - rock, paper, or scissors."
+    function getComputerChoice() {
+        switch (Math.floor(Math.random() * 3) + 1) {
+            case 1:
+                return "rock"
+            case 2:
+                return "paper"
+            case 3:
+                return "scissors"
+        }
     }
-    let totalScore = `${humanScore} - ${computerScore}`;   
-    return `${message} ${totalScore}`
-}
+    const computerChoice = getComputerChoice();
+    
+    function getHumanChoice() {
+        return humanChoice.toLowerCase();
+    }
+    const humanChoice = prompt("Choose rock, paper, or scissors:  ");
+    
+    function singleRound() { 
+        const win = "win";
+        const lose = "lose";
+        const draw = "draw"; 
+        const error = "error";
+        
+        if (getHumanChoice() == "rock" && computerChoice == "scissors") {
+            return win     
+        } if (getHumanChoice() == "paper" && computerChoice == "rock") {
+            return win
+        } if (getHumanChoice() == "scissors" && computerChoice == "paper") {
+            return win
+        } if (getHumanChoice() == computerChoice) {
+            return draw
+        } if (getHumanChoice() != "rock" && getHumanChoice() != "paper" && getHumanChoice() != "scissors") {
+            return error 
+        } else 
+            return lose
+    }
+    function scoring() {
+        // on a human win increment the humanScore by 1, on a loss increment computerScore by 1
+        // output both scores
+        const result = singleRound();
+        let message = "";
+        if (result == "win") {
+            humanScore ++;
+            message = `${getHumanChoice()} vs. ${computerChoice}. You win!`;
+        } if (result == "lose") {
+            computerScore ++;
+            message = `${getHumanChoice()} vs. ${computerChoice}. You lose.`;
+        } if (result == "draw") {
+            message = `${getHumanChoice()} vs. ${computerChoice}. Draw! Try again.`;
+        } if (result == "error") {
+            message = "Please input a vaild choice - rock, paper, or scissors."
+        }
+        let totalScore = `Score: ${humanScore} - ${computerScore}`;   
+        return `${message} ${totalScore}`
+    }
+    let humanScore = 0;
+    let computerScore = 0;
+    console.log(scoring());
 
-let humanScore = 0;
-let computerScore = 0;
-
-console.log(scoring());
+    
+}    
